c13d086d3a22b2bddb6cd83abb7b3d8131312b28
gfreemoon <deniskazet@gmail.com>
2025-08-18 22:14:41 +0300

    Update bbr.sh

--- OpenWRT/bbr.sh
+++ OpenWRT/bbr.sh
@@ -1,31 +1,22 @@
 #!/bin/sh
 
-# Устанавливаем локаль для корректного отображения русского текста
-export LANG=ru_RU.UTF-8
-export LC_ALL=ru_RU.UTF-8
-export LC_CTYPE=ru_RU.UTF-8
-if ! locale >/dev/null 2>&1; then
-    echo "Локаль ru_RU.UTF-8 не найдена. Русский текст может отображаться некорректно."
-    echo "Попробуйте установить пакет с локалями (opkg install locale) или проверить настройки терминала."
-fi
-
-# Устанавливаем модуль BBR
+# Installing BBR module
 opkg update
 opkg install kmod-tcp-bbr
 
-# Загружаем модуль BBR
+# Loading BBR module
 modprobe tcp_bbr
 
-# Устанавливаем BBR как алгоритм по умолчанию
+# Installing BBR as default
 sed -i '/tcp_congestion_control/d' /etc/sysctl.conf
 echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
 sysctl -w net.ipv4.tcp_congestion_control=bbr
 
-# Проверка текущего алгоритма и загрузки модуля
-echo "Текущий алгоритм:"
+# Test
+echo "Curent algorythm:"
 sysctl net.ipv4.tcp_congestion_control
 
-echo "Проверка модуля BBR:"
+echo "Testing BBR:"
 lsmod | grep tcp_bbr
 
-echo "Настройка завершена! Перезагрузите роутер и проверьте еще раз."
+echo "Setup complete! You should reboot your router and test again."
